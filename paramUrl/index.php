0223d01a7315d76d8ea9ad2986b4b0fa60687fca
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-24 13:34:27 +0200

    exercice paramUrl

--- paramUrl/index.php
+++ paramUrl/index.php
@@ -10,7 +10,7 @@
 </head>
 <body style="text-align:center;">
 
-    <h1 style="color:red; text-decoration:underline;">Loop</h1>
+    <h1 style="color:red; text-decoration:underline;">ParamUrl</h1>
 
     <h2>Exercice 1</h2>
     <?php include "exercice1.php";?>
